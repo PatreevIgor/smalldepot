4b47393f19e80cd8a56cdadac48b593042fa4fdb
Patreev Igor <igormanxxx@yandex.ru>
2015-02-26 14:06:14 +0300

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,9 +11,23 @@ Depot::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = { :host => 'www.myapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "patreevigor@gmail.com",
+    :password => "Superman87"
+  }
+
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
@@ -26,4 +40,6 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
 end
